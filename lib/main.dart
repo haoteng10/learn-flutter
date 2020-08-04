c86295aee6c024176ebc98185302d066819bdde7
Hao Teng <haoteng10@gmail.com>
2020-08-04 12:10:00 -0400

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      home: Scaffold(
-    appBar: AppBar(
-      title: Text("Learn Flutter"),
-      centerTitle: true,
-      backgroundColor: Colors.orange[600],
-    ),
-    body: Center(
-      child: Text(
-        "Text is right here!",
-        style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: "IndieFlower"),
+  runApp(MaterialApp(home: Home()));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Learn Flutter"),
+        centerTitle: true,
+        backgroundColor: Colors.orange[600],
       ),
-    ),
-    floatingActionButton:
-        FloatingActionButton(child: Text("Click"), onPressed: () => null),
-  )));
+      body: Center(child: Image.asset("assets/lake.jpg")),
+      floatingActionButton:
+          FloatingActionButton(child: Text("Click"), onPressed: () => null),
+    );
+  }
 }
