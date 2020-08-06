ca90efe1de843d044f76f8de4b2bfc4b0a502bd4
Hao Teng <haoteng10@gmail.com>
2020-08-06 15:37:47 -0400

    API Call

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+// import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:world_time_app/pages/home.dart';
+import 'package:world_time_app/pages/loading.dart';
+import 'package:world_time_app/pages/choose_location.dart';
 
 void main() {
-  runApp(MaterialApp(home: Home()));
+  runApp(MaterialApp(initialRoute: "/", routes: {
+    "/": (context) => Loading(),
+    "/home": (context) => Home(),
+    "/location": (context) => ChooseLocation()
+  }));
 }
